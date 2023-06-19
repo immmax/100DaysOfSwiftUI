4a97474f20c3e654612cb90e6150adc4af0d215b
Maxim Datskiy <datskiumax@gmail.com>
2023-06-19 14:43:03 -0500

    Code for Day 1

---
+++ swiftUI_day_1.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Cocoa
+
+var name = "Max"
+
+let charachter = "Daphnie"
+print(charachter)
+
+let multilineString = """
+well,
+this is not a hard thing
+to understand
+"""
+
+print(multilineString.count)
+
+print(charachter.uppercased())
+print(multilineString.hasPrefix("well"))
+print(charachter.hasSuffix("ie"))
+
+let score = 10
+let reallyBig = 100_000_000
+
+
