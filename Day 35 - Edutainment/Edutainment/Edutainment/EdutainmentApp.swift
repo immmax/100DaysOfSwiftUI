8a7b075c0eac698eec63d586ae04458ae6b43252
Maxim Datskiy <datskiumax@gmail.com>
2023-06-19 21:00:00 -0500

    Code for Day 35. Challenge - Edutainment. NOT FINISHED.

---
+++ Day 35 - Edutainment/Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Maxim Datskiy on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
