c847160692998ec808f7b7dd94a53f80e07f7084
Maxim Datskiy <datskiumax@gmail.com>
2023-06-19 20:54:09 -0500

    Code for Day 29-31. Project 5 - Word Scramble.

---
+++ Day 29 - WordScramble/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Maxim Datskiy on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
