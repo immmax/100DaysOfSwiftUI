be194c129a3fe0193c45c8edf1d25c90eaa562e6
Maxim Datskiy <datskiumax@gmail.com>
2023-06-19 19:02:44 -0500

    Code for Day 9

---
+++ swiftUI_day_9.playground/Contents.swift
@@ -0,0 +1,7 @@
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+var luckyNumbersReal = luckyNumbers.filter {!$0.isMultiple(of: 2)}.sorted {$0 < $1}.map {"\($0) is a lucky number"}
+
+for number in luckyNumbersReal {
+    print(number)
+}
