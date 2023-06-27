3b393cfda2cb52a9944c8514e1ccd1c1f5d7b194
Maxim Datskiy <datskiumax@gmail.com>
2023-06-27 18:41:18 -0500

    Code for Day 45. Part 3 - AnimationPair.

--- Day 43 - Drawing (intro)/Drawing/Drawing/Day-45-View.swift
+++ Day 43 - Drawing (intro)/Drawing/Drawing/Day-45-View.swift
@@ -135,12 +135,56 @@ struct Lesson2: View {
     }
 }
 
-//struct Lesson3: View {
-//
-//    var body: some View {
-//
-//    }
-//}
+struct Lesson3: View {
+    @State private var rows = 4
+    @State private var columns = 8
+    
+    struct Checkerboard: Shape {
+        var rows: Int
+        var columns: Int
+        
+        var animatableData: AnimatablePair<Double, Double> {
+            get {
+                AnimatablePair(Double(rows), Double(columns))
+            }
+            
+            set {
+                rows = Int(newValue.first)
+                columns = Int(newValue.second)
+            }
+        }
+        
+        func path(in rect: CGRect) -> Path {
+            var path = Path()
+            
+            let rowSize = rect.height / Double(rows)
+            let columnSize = rect.width / Double(columns)
+            
+            for row in 0..<rows {
+                for column in 0..<columns {
+                    if (row + column).isMultiple(of: 2) {
+                        let startX = columnSize * Double(column)
+                        let startY = rowSize * Double(row)
+                        
+                        let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                        path.addRect(rect)
+                    }
+                }
+            }
+            return path
+        }
+    }
+    
+    var body: some View {
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation (.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
+                }
+            }
+    }
+}
 
 
 struct Day_45_View: View {
@@ -164,14 +208,14 @@ struct Day_45_View: View {
                             NavigationLink {
                                 if number == 1 { Lesson1() }
                                 else if number == 2 { Lesson2() }
-//                                else if number == 3 { ContextMenu() }
+                                else if number == 3 { Lesson3() }
                                 else { Text("to be continued...") }
                             } label: {
                                 Text("Ex. \(number)")
                                     .padding()
                                     .frame(width: 80, height: 40)
                                     .background(.gray)
-                                    .foregroundColor(.red)
+                                    .foregroundColor(.cyan)
                                     .clipShape(RoundedRectangle(cornerRadius: 20))
                             }
                             .padding(.bottom)
