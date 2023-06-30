5193e2a94c2efac0c720591792d4abc0f455a3d6
Maxim Datskiy <datskiumax@gmail.com>
2023-06-30 13:27:42 -0500

    Little addition because of editing .gitignore. Added accets to Drawing project.

---
+++ Day 43 - Drawing (intro)/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Maxim Datskiy on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
