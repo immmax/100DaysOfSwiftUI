0b136a109fefa1379479c728cf035e8e08d9c83c
Maxim Datskiy <datskiumax@gmail.com>
2023-07-05 02:25:21 -0500

    Project 10 - Intro. Beginning of the project from scratch.

---
+++ Day 49 - Cupcake Corner/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Maxim Datskiy on 7/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
