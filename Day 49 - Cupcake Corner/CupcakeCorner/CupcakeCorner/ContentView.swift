1f826e319b4ee2e441b61681c4780540703be136
Maxim Datskiy <datskiumax@gmail.com>
2023-07-05 20:32:58 -0500

    Project 10. Cleaning after intro and preparing to the main part of the project.

--- Day 49 - Cupcake Corner/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Day 49 - Cupcake Corner/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,101 +7,15 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Max Datskiy"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
-}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    @State private var username = ""
-    @State private var email = ""
-    
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-                if let image = phase.image {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                } else if phase.error != nil {
-                    Text("There was an error liading an image.")
-                } else {
-                    ProgressView()
-                }
-            }
-            .frame(width: 200, height: 200)
-            
-            Form {
-                Section {
-                    TextField("Username", text: $username)
-                    TextField("Email", text: $email)
-                }
-                
-                Section {
-                    Button("Create") {
-                        print("Creating account...")
-                    }
-                }
-                .disabled(disableForm)
-            }
-            
-            List(results, id: \.trackId) { item in
-                VStack(alignment: .leading) {
-                    Text(item.trackName)
-                        .font(.headline.weight(.black))
-                    Text(item.collectionName)
-                }
-            }
-            .padding()
-            .task {
-                await loadData()
-            }
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
         }
-    }
-    
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Incalid data")
-        }
-    }
-    
-    var disableForm: Bool {
-        username.count < 5 || email.count < 5
+        .padding()
     }
 }
 
