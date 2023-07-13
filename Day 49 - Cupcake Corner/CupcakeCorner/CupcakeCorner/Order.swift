ab7884a7e28fcf4d0275b3a4e4c6e3b1601497bd
Maxim Datskiy <datskiumax@gmail.com>
2023-07-12 19:16:36 -0500

    Project 10. Day 50, Part 1/3 - Taking basic order details.

---
+++ Day 49 - Cupcake Corner/CupcakeCorner/CupcakeCorner/Order.swift
@@ -0,0 +1,26 @@
+//
+//  Order.swift
+//  CupcakeCorner
+//
+//  Created by Maxim Datskiy on 7/12/23.
+//
+
+import SwiftUI
+
+class Order: ObservableObject {
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    @Published var type = 0
+    @Published var quantity = 3
+    
+    @Published var specialRequestEnable = false {
+        didSet {
+            if specialRequestEnable == false {
+                extraFrosting = false
+                addSprinkles = false
+            }
+        }
+    }
+    @Published var extraFrosting = false
+    @Published var addSprinkles = false
+}
