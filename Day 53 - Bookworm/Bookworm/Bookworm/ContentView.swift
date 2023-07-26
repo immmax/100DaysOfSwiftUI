a77f50064ee764fbe68a059cba731ad3e503d486
Maxim Datskiy <datskiumax@gmail.com>
2023-07-26 15:17:08 -0500

    Project 11. Day 54, Part 1/3 - Creating books with Core Data.

--- Day 53 - Bookworm/Bookworm/Bookworm/ContentView.swift
+++ Day 53 - Bookworm/Bookworm/Bookworm/ContentView.swift
@@ -7,68 +7,33 @@
 
 import SwiftUI
 
-struct PushButton: View {
-    let title : String
-    @Binding var isOn: Bool
-    
-    var onColors = [Color.red, Color.yellow]
-    var offColors = [Color(white: 0.6), Color(white: 0.4)]
-    
-    var body: some View {
-        Button(title) {
-            isOn.toggle()
-        }
-        .padding()
-        .background(
-            LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom)
-        )
-        .foregroundColor(.white)
-        .clipShape(Capsule())
-        .shadow(radius: isOn ? 0 : 5)
-        
-    }
-    
-}
-
 struct ContentView: View {
-    @State private var rememberMe = false
-    
-    @AppStorage("notes") private var notes = ""
-    
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest (sortDescriptors: []) var students: FetchedResults<Student>
+    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    
+    @State private var showingAddScreen = false
     
     var body: some View {
         NavigationView {
             VStack {
-//                PushButton(title: "Remember Me", isOn: $rememberMe)
-//                Text(rememberMe ? "On" : "Off")
-//
-//                TextEditor(text: $notes)
-//                    .navigationTitle("Notes")
-//                    .padding()
-                
-                List(students) { student in
-                    Text("\(student.firstName ?? "Unknown") \(student.lastName ?? "Unknown")")
-                }
-                
-                Button("Add student") {
-                    let firstNames = ["Max", "Paul", "Sean", "Alex", "Dick", "Sally"]
-                    let lastNames = ["Hudson", "Allen", "Datskiy", "Wallace", "McGregor"]
-                    
-                    let chosenFirstName = firstNames.randomElement()!
-                    let chosenLastName = lastNames.randomElement()!
-                    
-                    let student = Student(context: moc)
-                    student.id = UUID()
-                    student.firstName = chosenFirstName
-                    student.lastName = chosenLastName
-                    
-                    try? moc.save()
+                Text("Count: \(books.count)")
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button() {
+                        showingAddScreen.toggle()
+                    } label: {
+                        Label( "Add Book", systemImage: "plus.circle.fill")
+                    }
                 }
             }
-            .padding()
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
         }
+        
+        
     }
 }
 
