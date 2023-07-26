9652ee24b0a7edd067fa04de325aa082ab833d61
Maxim Datskiy <datskiumax@gmail.com>
2023-07-26 03:25:46 -0500

    Project 11. Day 53 - Intro. Part 1/4 - Beginning of the project from scratch.

---
+++ Day 53 - Bookworm/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Maxim Datskiy on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
