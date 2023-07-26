22e1188f3bad0cf1d5d0d77398525d7ed3ec8120
Maxim Datskiy <datskiumax@gmail.com>
2023-07-26 04:23:11 -0500

    Project 11. Day 53 - Intro. Part 2/4 - Creating a custom component with @Binding

--- Day 53 - Bookworm/Bookworm/Bookworm/ContentView.swift
+++ Day 53 - Bookworm/Bookworm/Bookworm/ContentView.swift
@@ -7,13 +7,36 @@
 
 import SwiftUI
 
+struct PushButton: View {
+    let title : String
+    @Binding var isOn: Bool
+    
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+    
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(
+            LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom)
+        )
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
+        
+    }
+    
+}
+
 struct ContentView: View {
+    @State private var rememberMe = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
         }
         .padding()
     }
