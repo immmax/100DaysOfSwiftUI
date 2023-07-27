f6b1905f1f8ce4cabc2738a1d1f249f0c2e6a3b5
Maxim Datskiy <datskiumax@gmail.com>
2023-07-26 22:36:37 -0500

    Project 11. Day 55, Part 4/4 - Using an alert to pop a NavigationLink programmatically.

--- Day 53 - Bookworm/Bookworm/Bookworm/DetailView.swift
+++ Day 53 - Bookworm/Bookworm/Bookworm/DetailView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct DetailView: View {
     let book: Book
     
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.dismiss) var dismiss
+    @State private var showingDeleteAlert = false
     var body: some View {
         ScrollView {
             ZStack(alignment: .bottomTrailing) {
@@ -40,6 +43,26 @@ struct DetailView: View {
             
         }
         .navigationTitle(book.title ?? "Unknown Book")
-            .navigationBarTitleDisplayMode(.inline)
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("Delete book?", isPresented: $showingDeleteAlert) {
+            Button("Delete", role: .destructive, action: deleteBook)
+            Button("Cancel", role: .cancel) {}
+        } message: {
+            Text("Are you sure?")
+        }
+        .toolbar {
+            Button {
+                showingDeleteAlert = true
+            } label: {
+                Label("Delete this book", systemImage: "trash")
+            }
+        }
+    }
+    
+    func deleteBook() {
+        moc.delete(book)
+        
+        try? moc.save()
+        dismiss()
     }
 }
