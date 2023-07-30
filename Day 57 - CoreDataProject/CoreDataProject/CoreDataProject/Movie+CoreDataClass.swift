ae6188472792623e1e350e6d325f88c884e06849
Maxim Datskiy <datskiumax@gmail.com>
2023-07-29 19:41:54 -0500

    Project 12. Day 57, Part 3/5 - Creating NSManagedObject subclasses.

---
+++ Day 57 - CoreDataProject/CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Maxim Datskiy on 7/29/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+
+}
---
+++ Day 57 - CoreDataProject/CoreDataProject/CoreDataProject/Movie+CoreDataProperties.swift
@@ -0,0 +1,31 @@
+//
+//  Movie+CoreDataProperties.swift
+//  CoreDataProject
+//
+//  Created by Maxim Datskiy on 7/29/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Movie {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Movie> {
+        return NSFetchRequest<Movie>(entityName: "Movie")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var director: String?
+    @NSManaged public var year: Int16
+    
+    public var wrappedTitle: String {
+        title ?? "Unknown Title"
+    }
+
+}
+
+extension Movie : Identifiable {
+
+}
