47736f87d23b2e91667ad074b2a0a5b8d1c0e0e3
Maxim Datskiy <datskiumax@gmail.com>
2023-08-01 16:54:07 -0500

    Project 12. Day 58, Part 1/3 - Filtering @FetchRequest using NSPredicate.

--- Day 57 - CoreDataProject/CoreDataProject/CoreDataProject/ContentView.swift
+++ Day 57 - CoreDataProject/CoreDataProject/CoreDataProject/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Maxim Datskiy on 7/29/23.
 //
 
+import CoreData
 import SwiftUI
 
 struct Student: Hashable {
@@ -16,46 +17,82 @@ struct ContentView: View {
     
     @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
     
+    @FetchRequest(sortDescriptors: [], predicate: NSPredicate(format: "NOT name BEGINSWITH[c] %@", "e")) var ships: FetchedResults<Ship>
+    
     @State private var wizardName = ""
     
     let students = [Student(name: "Michael Scott"), Student(name: "Jim Halpert")]
     
     var body: some View {
         VStack {
-            Section("Enter wizard's name") {
-                TextField("Wizard's Name", text: $wizardName)
-            }
-            
-            Section {
-                List(wizards, id: \.self) { wizard in
-                    Text(wizard.name ?? "Unknown")
+            Section("WIZARDS") {
+                Section("Enter wizard's name") {
+                    TextField("Wizard's Name", text: $wizardName)
+                }
+                
+                Section {
+                    List(wizards, id: \.self) { wizard in
+                        Text(wizard.name ?? "Unknown")
+                    }
+                }
+                
+                
+                Section {
+                    HStack {
+                        Button("Add Wizard") {
+                            let wizard = Wizard(context: moc)
+                            wizard.name = wizardName
+                            
+                            wizardName = ""
+                        }
+                        .buttonStyle(.bordered)
+                        .padding(.horizontal)
+                        
+                        Button("Save") {
+                            do {
+                                if moc.hasChanges {
+                                    try moc.save()
+                                }
+                                wizardName = ""
+                            } catch {
+                                print(error.localizedDescription)
+                            }
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .padding(.horizontal)
+                    }
                 }
             }
-            
-            
-            Section {
-                HStack {
-                    Button("Add") {
-                        let wizard = Wizard(context: moc)
-                        wizard.name = wizardName
-                        
-                        wizardName = ""
-                    }
-                    .buttonStyle(.bordered)
-                    .padding(.horizontal)
-                    
-                    Button("Save") {
-                        do {
-                            if moc.hasChanges {
-                                try moc.save()
-                            }
-                            wizardName = ""
-                        } catch {
-                            print(error.localizedDescription)
+            Section("SHIPS") {
+                Section("Ships list") {
+                    List {
+                        ForEach(ships, id: \.self) { ship in
+                            Text(ship.name ?? "Unknown name")
                         }
                     }
+                }
+                
+                Section("Ship's control") {
+                    Button("Add examples") {
+                        let ship1 = Ship(context: moc)
+                        ship1.name = "Enterprise"
+                        ship1.universe = "Star Trek"
+                        
+                        let ship2 = Ship(context: moc)
+                        ship2.name = "Defiant"
+                        ship2.universe = "Star Trek"
+                        
+                        let ship3 = Ship(context: moc)
+                        ship3.name = "Millenium Falcon"
+                        ship3.universe = "Star Wars"
+                        
+                        let ship4 = Ship(context: moc)
+                        ship4.name = "Executor"
+                        ship4.universe = "Star Wars"
+                        
+                        try? moc.save()
+                    }
                     .buttonStyle(.borderedProminent)
-                    .padding(.horizontal)
                 }
             }
         }
