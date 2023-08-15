3feae9367c3f6e0b7cd9f021ac2022f822368b0f
Maxim Datskiy <datskiumax@gmail.com>
2023-08-14 20:01:47 -0500

    Create new project - Friends (Day 60)
    This project is a Milestone for projects 10-12

---
+++ Day 59 - Milestone - Friends/Friends/Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Maxim Datskiy on 8/14/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
