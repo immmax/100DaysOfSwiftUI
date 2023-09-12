1b20e8ae414df9452c0c4c4b2726d3b98d2df5d1
Maxim Datskiy <datskiumax@gmail.com>
2023-09-11 17:37:48 -0700

    Project 13 - Instafilter - Day 61, Part 1/4 - Introduction
    Создание нового проекта.

---
+++ Day 62 - Instafilter/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Maxim Datskiy on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
