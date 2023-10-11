04d6cce5582413d16a69f642bd052637c86d6532
Maxim Datskiy <datskiumax@gmail.com>
2023-10-11 02:22:18 -0500

    Project 14 - BucketList - Day 68, Part 4/4 - Switching view states with enums

--- Day 68 - BucketList/BucketList/BucketList/ContentView.swift
+++ Day 68 - BucketList/BucketList/BucketList/ContentView.swift
@@ -7,28 +7,40 @@
 
 import SwiftUI
 
+enum LoadingState {
+    case loading, success,failed
+}
+
+struct LoadingView: View {
+    var body: some View {
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
 struct ContentView: View {
+    var loadingState = LoadingState.loading
     
     var body: some View {
-        Text("Hello World")
-            .onTapGesture {
-                let str = "Test Message"
-                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                
-                do {
-                    try str.write(to: url, atomically: true, encoding: .utf8)
-                    
-                    let input = try String(contentsOf: url)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-    }
-    
-    func getDocumentsDirectory() -> URL {
-        let path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return path[0]
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
+        }
     }
 }
 
