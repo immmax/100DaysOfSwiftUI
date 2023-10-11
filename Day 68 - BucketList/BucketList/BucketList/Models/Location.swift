00d7e9d2d8b270501bd9ed267ddac2d3ebf74bcb
Maxim Datskiy <datskiumax@gmail.com>
2023-10-11 04:08:39 -0500

    Project 14 - BucketList - Day 70, Part 2/3 - Improving our map annotations

--- Day 68 - BucketList/BucketList/BucketList/Models/Location.swift
+++ Day 68 - BucketList/BucketList/BucketList/Models/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Location: Identifiable, Codable, Equatable {
     let id: UUID
@@ -13,4 +14,19 @@ struct Location: Identifiable, Codable, Equatable {
     var description: String
     let latitude: Double
     let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude,
+                               longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(),
+                                  name: "Buckingham Palace",
+                                  description: "Where Queen Elizabeth lived with her dorgis",
+                                  latitude: 51.501,
+                                  longitude: -0.141)
+    
+    static func ==(lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
