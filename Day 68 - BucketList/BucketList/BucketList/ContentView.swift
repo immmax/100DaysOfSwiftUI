c3e290077981f8832d72a535db6ce4da6b1dd746
Maxim Datskiy <datskiumax@gmail.com>
2023-10-11 02:55:35 -0500

    Project 14 - BucketList - Day 69, Part 1/2 - Integrating MapKit with SwiftUI
    Code should look somehow different for iOS 17

--- Day 68 - BucketList/BucketList/BucketList/ContentView.swift
+++ Day 68 - BucketList/BucketList/BucketList/ContentView.swift
@@ -5,41 +5,45 @@
 //  Created by Maxim Datskiy on 9/20/23.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success,failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    var loadingState = LoadingState.loading
+    @State private var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.1, longitude: -0.12),
+        span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    
+    let locations = [
+        Location(name: "Buckingham Palace",
+                 coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London",
+                 coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+    //            MapMarker(coordinate: location.coordinate)
+                MapAnnotation(coordinate: location.coordinate) {
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+//                            .onTapGesture {
+//                                print("Tapped on \(location.name)")
+//                            }
+                    }
+                }
+            }
+            .navigationTitle("London Explorer")
         }
     }
 }
