d89619ea4c5025c5374784c9db775ecc98303115
Maxim Datskiy <datskiumax@gmail.com>
2023-10-15 19:18:01 -0500

    Project 14 - BucketList - Day 72, Part 2/2 - Locking our UI behind Face ID

--- Day 68 - BucketList/BucketList/BucketList/Views/ContentView.swift
+++ Day 68 - BucketList/BucketList/BucketList/Views/ContentView.swift
@@ -12,77 +12,63 @@ import SwiftUI
 struct ContentView: View {
     @StateObject private var viewModel = ViewModel()
     var body: some View {
-        ZStack {
-            Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
-                MapAnnotation(coordinate: location.coordinate) {
-                    VStack {
-                        Image(systemName: "star.circle")
-                            .resizable()
-                            .foregroundColor(.red)
-                            .frame(width: 44, height: 44)
-                            .background(.white)
-                            .clipShape(Circle())
-                        Text(location.name)
-                            .fixedSize()
-                    }
-                    .onTapGesture {
-                        viewModel.selectedPlace = location
+        if viewModel.isUnlocked {
+            ZStack {
+                Map(coordinateRegion: $viewModel.mapRegion, annotationItems: viewModel.locations) { location in
+                    MapAnnotation(coordinate: location.coordinate) {
+                        VStack {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+                            Text(location.name)
+                                .fixedSize()
+                        }
+                        .onTapGesture {
+                            viewModel.selectedPlace = location
+                        }
                     }
                 }
-            }
-            .ignoresSafeArea()
-            
-            Circle()
-                .fill(.blue)
-                .opacity(0.3)
-                .frame(width: 32, height: 32)
-            
-            VStack {
-                Spacer()
+                .ignoresSafeArea()
                 
-                HStack {
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                
+                VStack {
                     Spacer()
                     
-                    Button {
-                        viewModel.addLocation()
-                    } label: {
-                        Image(systemName: "plus")
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            viewModel.addLocation()
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
-                    .padding()
-                    .background(.black.opacity(0.75))
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .clipShape(Circle())
-                    .padding(.trailing)
                 }
             }
-        }
-        .sheet(item: $viewModel.selectedPlace) { place in
-            EditView(location: place) { newLocation in
-                viewModel.update(location: newLocation)
-            }
-        } 
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "We need to unlock your data"
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
-                                   localizedReason: reason) { success, autenticationError in
-                if success {
-                    // authenticated succesfully
-                    viewModel.isUnlocked = true
-                } else {
-                    // there was a problem
+            .sheet(item: $viewModel.selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    viewModel.update(location: newLocation)
                 }
             }
         } else {
-            // no biometrics
-            
+            Button("Unlock places", action: viewModel.authenticate)
+                .padding()
+                .background(.blue)
+                .foregroundColor(.white)
+                .clipShape(Capsule())
         }
     }
 }
--- Day 68 - BucketList/BucketList/ViewModels/ContentView-ViewModel.swift
+++ Day 68 - BucketList/BucketList/ViewModels/ContentView-ViewModel.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
@@ -13,11 +14,8 @@ extension ContentView {
         @Published var mapRegion = MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 50, longitude: 0),
             span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-        
         @Published private(set) var locations: [Location]
-        
         @Published var selectedPlace: Location?
-        
         @Published var isUnlocked = false
         
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
@@ -58,6 +56,30 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+                let reason = "Please authenticate yourself to unlock your places."
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics,
+                                       localizedReason: reason) { success, autenticationError in
+                    if success {
+                        // authenticated succesfully
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        // there was a problem
+                    }
+                }
+            } else {
+                // no biometrics
+                
             }
         }
     }
