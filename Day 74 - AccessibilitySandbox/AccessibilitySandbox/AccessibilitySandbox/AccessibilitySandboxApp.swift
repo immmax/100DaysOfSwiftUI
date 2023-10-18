eeab4d197d39a20a9d15ee239df1fa7b6fcb5f0e
Maxim Datskiy <datskiumax@gmail.com>
2023-10-18 04:15:07 -0500

    Project 15 - AccessibilitySandbox - Day 74, Part 1/4 - Introduction
    Create new Xcode project

---
+++ Day 74 - AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Maxim Datskiy on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
