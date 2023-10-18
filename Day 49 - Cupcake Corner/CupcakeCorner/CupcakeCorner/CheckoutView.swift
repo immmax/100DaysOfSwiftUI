97278a085ef7042f88a9aadc943fde977ab423b7
Maxim Datskiy <datskiumax@gmail.com>
2023-10-18 06:39:38 -0500

    Project 15 - AccessibilitySandbox - Day 76, Challenge, Part 1/3
    Add Accessibility to Cupcake Corner
    The check out view in Cupcake Corner uses an image and loading spinner that don’t add anything to the UI, so find a way to make the screenreader not read them out.

--- Day 49 - Cupcake Corner/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ Day 49 - Cupcake Corner/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -26,6 +26,7 @@ struct CheckoutView: View {
                     ProgressView()
                 }
                 .frame(height: 233)
+                .accessibilityHidden(true)
                 
                 Text("Your total is \(order.details.cost, format: .currency(code: "USD"))")
                     .font(.title)
