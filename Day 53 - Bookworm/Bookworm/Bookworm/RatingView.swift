fbf90453f080b21b1221ed8d982def9e98a366a3
Maxim Datskiy <datskiumax@gmail.com>
2023-10-18 06:13:21 -0500

    Project 15 - AccessibilitySandbox - Day 75, Part 3/3 - Fixing Bookworm (v2)

--- Day 53 - Bookworm/Bookworm/Bookworm/RatingView.swift
+++ Day 53 - Bookworm/Bookworm/Bookworm/RatingView.swift
@@ -32,9 +32,19 @@ struct RatingView: View {
                     .onTapGesture {
                         rating = number
                     }
-                    .accessibilityLabel("\(number == 1 ? "1 star" : "\(number) stars")")
-                    .accessibilityRemoveTraits(.isImage)
-                    .accessibilityAddTraits(number > rating ? .isButton : [.isButton, .isSelected])
+            }
+        }
+        .accessibilityElement()
+        .accessibilityLabel("Rating")
+        .accessibilityValue(rating == 1 ? "1 star" : "\(rating) stars")
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .decrement:
+                if rating > 1 { rating -= 1 }
+            case .increment:
+                if rating < maximumRating { rating += 1 }
+            default:
+                break
             }
         }
     }
