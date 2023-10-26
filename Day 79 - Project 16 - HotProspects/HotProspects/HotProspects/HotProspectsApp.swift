1288012a9717e4e788d16e48f46f2d104f095a30
Maxim Datskiy <datskiumax@gmail.com>
2023-10-26 02:57:34 -0500

    Project 16 - HotProspects - Day 79, Part 1/3 - Introduction
    Create new Xcode project

---
+++ Day 79 - Project 16 - HotProspects/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Maxim Datskiy on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
