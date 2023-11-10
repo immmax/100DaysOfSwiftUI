891449a0ab39704c6bcab1e3dddf2d2081460d91
Maxim Datskiy <datskiumax@gmail.com>
2023-11-09 16:11:01 -0800

    Project 16 - HotProspects - Day 81, Part 4/4 - Adding Swift package dependencies in Xcode

--- Day 79 - Project 16 - HotProspects/HotProspects/HotProspects/ContentView.swift
+++ Day 79 - Project 16 - HotProspects/HotProspects/HotProspects/ContentView.swift
@@ -5,38 +5,21 @@
 //  Created by Maxim Datskiy on 10/26/23.
 //
 
+import SamplePackage
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
+    let possibleNumbers = Array(1...60)
     
     var body: some View {
-        VStack {
-            Button("Request Permittion") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
-                    if success {
-                        print("All set!")
-                    } else if let error = error {
-                        print(error.localizedDescription)
-                    }
-                }
-            }
-            .buttonStyle(.bordered)
-            
-            Button("Schedule Notification") {
-                let content = UNMutableNotificationContent()
-                content.title = "Water your plants!"
-                content.subtitle = "They look thirsty"
-                content.sound = UNNotificationSound.default
-                
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-            .buttonStyle(.borderedProminent)
-        }
+        Text(results)
+    }
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        
+        return strings.joined(separator: ", ")
     }
 }
 
