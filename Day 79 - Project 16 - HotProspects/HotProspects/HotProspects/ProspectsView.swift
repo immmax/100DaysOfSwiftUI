682f4558b40f51362b0b06b2b6485a3e3b69b699
Maxim Datskiy <datskiumax@gmail.com>
2024-01-01 04:17:30 -0800

    Project 16 - HotProspects - Day 83, Part 3/3 - Adding options with swipe actions

--- Day 79 - Project 16 - HotProspects/HotProspects/HotProspects/ProspectsView.swift
+++ Day 79 - Project 16 - HotProspects/HotProspects/HotProspects/ProspectsView.swift
@@ -28,6 +28,23 @@ struct ProspectsView: View {
                         Text(prospect.email)
                             .foregroundColor(.secondary)
                     }
+                    .swipeActions {
+                        if prospect.isContacted {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Uncontacted", systemImage: "person.crop.circle.badge.xmark")
+                            }
+                            .tint(.blue)
+                        } else {
+                            Button {
+                                prospects.toggle(prospect)
+                            } label: {
+                                Label("Mark Contacted", systemImage: "person.crop.circle.fill.badge.checkmark")
+                            }
+                            .tint(.green)
+                        }
+                    }
                 }
             }
             .navigationTitle(title)
