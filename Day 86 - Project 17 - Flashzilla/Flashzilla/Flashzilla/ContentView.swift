86c9be4f2c78c7c42ee555a9b5651c3e65ac149b
Maxim Datskiy <datskiumax@gmail.com>
2024-03-01 22:38:59 -0800

    Project 16 - FlashZilla - Day 86, Part 3/3 - Disabling user interactivity with allowsHitTesting()

--- Day 86 - Project 17 - Flashzilla/Flashzilla/Flashzilla/ContentView.swift
+++ Day 86 - Project 17 - Flashzilla/Flashzilla/Flashzilla/ContentView.swift
@@ -11,40 +11,74 @@ struct ContentView: View {
 //    @State private var currentAmount = 0.0
 //    @State private var finalAmount = 1.0
     
-    @State private var currentAmount = Angle.zero
-    @State private var finalAmount = Angle.zero
-    
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+//    @State private var currentAmount = Angle.zero
+//    @State private var finalAmount = Angle.zero
+//    
+//    @State private var offset = CGSize.zero
+//    @State private var isDragging = false
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
+        // part 3 below
+        VStack {
+            Text("Hello")
+            
+            Spacer()
+                .frame(height: 100)
+            
+            Text("World")
+        }
+        .contentShape(.rect)
+        .onTapGesture {
+            print("VStack tapped!")
+        }
         
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
+//        ZStack {
+//            Rectangle()
+//                .fill(.blue)
+//                .frame(width: 300, height: 300)
+//                .onTapGesture {
+//                    print("Rectangle tapped!")
+//                }
+//            
+//            Circle()
+//                .fill(.red)
+//                .frame(width: 300, height: 300)
+//                .contentShape(.rect)
+//                .onTapGesture {
+//                    print("Circle tapped!")
+//                }
+//                .allowsHitTesting(false)
+//        }
         
-        let combined = pressGesture.sequenced(before: dragGesture)
+        // part 2 below
+//        let dragGesture = DragGesture()
+//            .onChanged { value in
+//                offset = value.translation
+//            }
+//            .onEnded { _ in
+//                withAnimation {
+//                    offset = .zero
+//                    isDragging = false
+//                }
+//            }
+//        
+//        let pressGesture = LongPressGesture()
+//            .onEnded { value in
+//                withAnimation {
+//                    isDragging = true
+//                }
+//            }
+//        
+//        let combined = pressGesture.sequenced(before: dragGesture)
+//        
+//        Circle()
+//            .fill(.red)
+//            .frame(width: 64, height: 64)
+//            .scaleEffect(isDragging ? 1.5 : 1)
+//            .offset(offset)
+//            .gesture(combined)
         
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
-        
+        // part 1 below
 //        VStack {
 //            Text("Hello, world!")
 //                .onTapGesture {
