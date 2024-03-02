af7a6565595ee9d9c9094c4b50b9898b11a33bbd
Maxim Datskiy <datskiumax@gmail.com>
2024-03-01 18:23:49 -0800

    Project 16 - FlashZilla - Day 86, Part 1/3 - Introduction
    Create new Xcode project

---
+++ Day 86 - Project 17 - Flashzilla/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Maxim Datskiy on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
