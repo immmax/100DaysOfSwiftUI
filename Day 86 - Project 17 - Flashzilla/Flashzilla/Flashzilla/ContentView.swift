8c4b6873a1ec88d1009ebf250f7cde8cb24699c5
Maxim Datskiy <datskiumax@gmail.com>
2024-03-02 01:51:37 -0800

    Project 16 - FlashZilla - Day 88, Part 2/3 - Building a stack of cards

--- Day 86 - Project 17 - Flashzilla/Flashzilla/Flashzilla/ContentView.swift
+++ Day 86 - Project 17 - Flashzilla/Flashzilla/Flashzilla/ContentView.swift
@@ -7,9 +7,31 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+    
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
