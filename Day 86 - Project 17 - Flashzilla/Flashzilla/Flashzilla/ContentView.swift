4bdd6ec68ce608d8741281a55a6c9b5185ca72ed
Maxim Datskiy <datskiumax@gmail.com>
2024-03-02 14:42:20 -0800

    Project 17 - FlashZilla - Day 89, Part 3/3 - Ending the app with allowsHitTesting()

--- Day 86 - Project 17 - Flashzilla/Flashzilla/Flashzilla/ContentView.swift
+++ Day 86 - Project 17 - Flashzilla/Flashzilla/Flashzilla/ContentView.swift
@@ -49,6 +49,15 @@ struct ContentView: View {
                         .stacked(at: index, in: cards.count)
                     }
                 }
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: reserCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                }
             }
             
             if accessibilityDifferentiateWithoutColor {
@@ -85,7 +94,9 @@ struct ContentView: View {
         }
         .onChange(of: scenePhase) {
             if scenePhase == .active {
-                isActive = true
+                if cards.isEmpty == false {
+                    isActive = true
+                }
             } else {
                 isActive = false
             }
@@ -94,6 +105,16 @@ struct ContentView: View {
     
     func removeCard(at index: Int) {
         cards.remove(at: index)
+        
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+    
+    func reserCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
     }
 }
 
