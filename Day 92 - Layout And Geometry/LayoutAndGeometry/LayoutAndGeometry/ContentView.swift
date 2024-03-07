a0314f8e0a5a057aedd410a651397af2ed4d40fd
Maxim Datskiy <datskiumax@gmail.com>
2024-03-06 23:08:55 -0800

    Project 18 - Layout and geometry - Day 92, Part 1/5 - Introduction
    Create a new Xcode project

---
+++ Day 92 - Layout And Geometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Maxim Datskiy on 3/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 92 - Layout And Geometry/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Maxim Datskiy on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
