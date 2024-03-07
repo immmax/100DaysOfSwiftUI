08f77d1af3578eeb6e802648ca465f73c05573da
Maxim Datskiy <datskiumax@gmail.com>
2024-03-07 00:29:53 -0800

    Project 18 - Layout and geometry - Day 92, Part 4/5 - How to create a custom alignment guide

--- Day 92 - Layout And Geometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Day 92 - Layout And Geometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,23 +7,63 @@
 
 import SwiftUI
 
+//        PART 4 - How to create a custom alignment guide
+extension VerticalAlignment {
+    enum MidAccountAndName: AlignmentID {
+        static func defaultValue(in context: ViewDimensions) -> CGFloat {
+            context[.top]
+        }
+    }
+    
+    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
 struct ContentView: View {
     var body: some View {
 //        DAY 1
         
-//        PART 3 - Alignment and alignment guides
-
-        VStack(alignment: .leading) {
-            ForEach(1..<10) { position in
-                Text("Number \(position)")
-                    .alignmentGuide(.leading, computeValue: { _ in
-                        Double(position) * -10
+//        PART 4 - How to create a custom alignment guide
+        
+        HStack(alignment: .midAccountAndName) {
+            VStack {
+                Text("Twitter")
+                    .font(.largeTitle)
+                Text("@immmax")
+                    .alignmentGuide(.midAccountAndName, computeValue: { d in
+                        d[VerticalAlignment.center]
                     })
+                
+                Image(.dumb)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+            }
+            
+            VStack {
+                Text("No way")
+                Text("Full name:")
+                
+                Text("JAMES CARRY")
+                    .font(.largeTitle)
+                    .alignmentGuide(.midAccountAndName, computeValue: { d in
+                        d[VerticalAlignment.center]
+                    })
+                Text("Kidding me?")
             }
         }
-        .background(.red)
-        .frame(width: 400, height: 400)
-        .background(.cyan)
+        
+//        PART 3 - Alignment and alignment guides
+
+//        VStack(alignment: .leading) {
+//            ForEach(1..<10) { position in
+//                Text("Number \(position)")
+//                    .alignmentGuide(.leading, computeValue: { _ in
+//                        Double(position) * -10
+//                    })
+//            }
+//        }
+//        .background(.red)
+//        .frame(width: 400, height: 400)
+//        .background(.cyan)
         
 //        VStack(alignment: .leading) {
 //            Text("Hello World")
