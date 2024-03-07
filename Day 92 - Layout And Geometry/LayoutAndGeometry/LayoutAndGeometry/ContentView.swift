f9e62af819edf67f3924c7740ee11a0c2185d1fd
Maxim Datskiy <datskiumax@gmail.com>
2024-03-07 00:12:30 -0800

    Project 18 - Layout and geometry - Day 92, Part 3/5 - Alignment and alignment guides

--- Day 92 - Layout And Geometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ Day 92 - Layout And Geometry/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -11,9 +11,53 @@ struct ContentView: View {
     var body: some View {
 //        DAY 1
         
+//        PART 3 - Alignment and alignment guides
+
+        VStack(alignment: .leading) {
+            ForEach(1..<10) { position in
+                Text("Number \(position)")
+                    .alignmentGuide(.leading, computeValue: { _ in
+                        Double(position) * -10
+                    })
+            }
+        }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.cyan)
+        
+//        VStack(alignment: .leading) {
+//            Text("Hello World")
+//                .alignmentGuide(.leading, computeValue: { d in
+//                    d[.trailing]
+//                })
+//
+//            Text("Placeholder with a longer text line")
+//        }
+//        .background(.red)
+//        .frame(width: 400, height: 400)
+//        .background(.cyan)
+        
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("Live")
+//                .font(.caption)
+//            
+//            Text("long")
+//            
+//            Text("and")
+//                .font(.title)
+//            
+//            Text("prosper")
+//                .font(.largeTitle)
+//        }
+        
+//        Text("Live long and prosper")
+//            .foregroundStyle(.white)
+//            .frame(width: 300, height: 300, alignment: .leading)
+//            .background(.cyan)
+        
 //        PART 2 - How layout works in SwiftUI
         
-        Color.cyan
+//        Color.cyan
         
 //        Text("Hello, world!")
 //            .padding()
