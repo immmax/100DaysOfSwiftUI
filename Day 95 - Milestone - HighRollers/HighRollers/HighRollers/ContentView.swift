6f85126249c48a9a8a253ad13f1be5f9b2691e3e
Maxim Datskiy <datskiumax@gmail.com>
2024-03-11 15:44:14 -0700

    Day 95 - Milestone: Projects 16-18 - Create the new project

---
+++ Day 95 - Milestone - HighRollers/HighRollers/HighRollers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HighRollers
+//
+//  Created by Maxim Datskiy on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day 95 - Milestone - HighRollers/HighRollers/HighRollers/HighRollersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighRollersApp.swift
+//  HighRollers
+//
+//  Created by Maxim Datskiy on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HighRollersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
