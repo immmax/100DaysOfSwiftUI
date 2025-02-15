220d3566bfd60cfbaab9fa38def2ad02b6584c50
Maxim Datskiy <datskiumax@gmail.com>
2025-02-15 14:32:35 -0800

    iOS 17 UPDATE - Days 58 - Part 1
    Dynamically sorting and filtering @Query with SwiftUI

--- Day 57 - SwiftDataProject/SwiftDataProject/SwiftDataProject/ContentView.swift
+++ Day 57 - SwiftDataProject/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -10,23 +10,16 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query(filter: #Predicate<User> { user in
-        if user.name.localizedStandardContains("S") {
-            if user.city == "Seattle, WA" {
-                return true
-            } else {
-                return false
-            }
-        } else {
-            return false
-        }
-    }, sort: \User.name) var users: [User]
+    @State private var showingUpcomingOnly = false
+    
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
     
     var body: some View {
         NavigationStack {
-            List(users) { user in
-                Text(user.name)
-            }
+            UsersView(minimumJoinDate: showingUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
             .navigationTitle("Users")
             .toolbar {
                 Button("Add Samples", systemImage: "plus") {
@@ -40,6 +33,25 @@ struct ContentView: View {
                     modelContext.insert(second)
                     modelContext.insert(third)
                     modelContext.insert(forth)
+                }
+                
+                Button(showingUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
+                    showingUpcomingOnly.toggle()
+                }
+                
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate)
+                            ])
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name)
+                            ])
+                    }
                 }
             }
         }
---
+++ Day 57 - SwiftDataProject/SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -0,0 +1,30 @@
+//
+//  UsersView.swift
+//  SwiftDataProject
+//
+//  Created by Maxim Datskiy on 2/14/25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct UsersView: View {
+    @Query var users: [User]
+    
+    var body: some View {
+        List(users) { user in
+            Text(user.name)
+        }
+    }
+    
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
+        _users = Query(filter: #Predicate<User> { user in
+            user.joinDate >= minimumJoinDate
+        }, sort: sortOrder)
+    }
+}
+
+#Preview {
+    UsersView(minimumJoinDate: .now, sortOrder: [SortDescriptor(\User.name)])
+        .modelContainer(for: User.self)
+}
