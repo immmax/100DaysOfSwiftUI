ef4c8091218ff12c5fce135189283a3a7ff6d125
Maxim Datskiy <datskiumax@gmail.com>
2025-02-15 14:47:21 -0800

    iOS 17 UPDATE - Days 58 - Part 2
    Relationships with SwiftData, SwiftUI and @Query

---
+++ Day 57 - SwiftDataProject/SwiftDataProject/SwiftDataProject/Job.swift
@@ -0,0 +1,22 @@
+//
+//  Job.swift
+//  SwiftDataProject
+//
+//  Created by Maxim Datskiy on 2/15/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Job {
+    var name: String
+    var priority: Int
+    var owner: User?
+    
+    init(name: String, priority: Int, owner: User? = nil) {
+        self.name = name
+        self.priority = priority
+        self.owner = owner
+    }
+}
