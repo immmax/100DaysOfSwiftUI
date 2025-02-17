3777ac59ce56f4b4082e0a040cfe877f45a12aeb
Maxim Datskiy <datskiumax@gmail.com>
2025-02-16 23:50:20 -0800

    iOS 17 UPDATE - Day 66 - InstaFilter (last - 65, typo)

--- Day 62 - Instafilter/Instafilter/Instafilter/ContentView.swift
+++ Day 62 - Instafilter/Instafilter/Instafilter/ContentView.swift
@@ -8,11 +8,15 @@
 import CoreImage
 import CoreImage.CIFilterBuiltins
 import PhotosUI
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
     @State private var selectedItem: PhotosPickerItem?
     @State private var processedImage: Image?
+    
+    @AppStorage("filterCount") var filterCount = 0
+    @Environment(\.requestReview) var requestReview
     
     @State private var filterIntensity = 0.5
     @State private var filterRadius = 100.0
@@ -21,7 +25,6 @@ struct ContentView: View {
     
     @State private var showingFilterSheet = false
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
-    
     let context = CIContext()
     
     var body: some View {
@@ -38,15 +41,15 @@ struct ContentView: View {
                         ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
                     }
                 }
-                //                .buttonStyle(.plain)
+//                .buttonStyle(.plain)
                 
                 Spacer()
                 
-                if currentFilter.inputKeys.contains(kCIInputIntensityKey){
+                if currentFilter.inputKeys.contains(kCIInputIntensityKey) {
                     HStack {
                         Text("Intensity")
                         Slider(value: $filterIntensity, in: 0...1)
-                            .onChange(of: filterIntensity) { applyProcessing() }
+                            .onChange(of: filterIntensity, applyProcessing)
                     }
                     .padding(.vertical)
                 }
@@ -55,7 +58,7 @@ struct ContentView: View {
                     HStack {
                         Text("Radius")
                         Slider(value: $filterRadius, in: 0...200)
-                            .onChange(of: filterRadius) { applyProcessing() }
+                            .onChange(of: filterRadius, applyProcessing)
                     }
                     .padding(.vertical)
                 }
@@ -64,7 +67,7 @@ struct ContentView: View {
                     HStack {
                         Text("Scale")
                         Slider(value: $filterScale, in: 0.5...200)
-                            .onChange(of: filterScale) { applyProcessing() }
+                            .onChange(of: filterScale, applyProcessing)
                     }
                     .padding(.vertical)
                 }
@@ -76,10 +79,9 @@ struct ContentView: View {
                     
                     Spacer()
                     
-//                    Button("Save", action: save)
-//                        .buttonStyle(.borderedProminent)
-//                        .tint(image == nil ? .none : .cyan)
-//                        .disabled(image == nil)
+                    if let processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                    }
                 }
             }
             .padding([.horizontal, .bottom])
@@ -87,16 +89,16 @@ struct ContentView: View {
         }
         .onChange(of: selectedItem, loadImage)
         
-        .confirmationDialog("Select the filter", isPresented: $showingFilterSheet) {
-            Button("Crystallize")      { setFilter(CIFilter.crystallize()) }
-            Button("Edges")             { setFilter(CIFilter.edges()) }
-            Button("Gaussian Blur")     { setFilter(CIFilter.gaussianBlur()) }
-            Button("Pixellate")         { setFilter(CIFilter.pixellate()) }
-            Button("Sepia Tone")        { setFilter(CIFilter.sepiaTone()) }
-            Button("Unsharp Mask")      { setFilter(CIFilter.unsharpMask()) }
-            Button("Vignette")          { setFilter(CIFilter.vignette()) }
-            Button("Pointillize")       { setFilter(CIFilter.pointillize()) }
-            Button("Vibrance")          { setFilter(CIFilter.vibrance()) }
+        .confirmationDialog("Select a filter", isPresented: $showingFilterSheet) {
+            Button("Crystallize")   { setFilter(CIFilter.crystallize()) }
+            Button("Edges")         { setFilter(CIFilter.edges()) }
+            Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+            Button("Pixellate")     { setFilter(CIFilter.pixellate()) }
+            Button("Sepia Tone")    { setFilter(CIFilter.sepiaTone()) }
+            Button("Unsharp Mask")  { setFilter(CIFilter.unsharpMask()) }
+            Button("Vignette")      { setFilter(CIFilter.vignette()) }
+            Button("Pointillize")   { setFilter(CIFilter.pointillize()) }
+            Button("Vibrance")      { setFilter(CIFilter.vibrance()) }
             Button("Cancel", role: .cancel) { }
         }
     }
@@ -136,26 +138,15 @@ struct ContentView: View {
         showingFilterSheet = true
     }
     
-    func setFilter(_ filter: CIFilter) {
+    @MainActor func setFilter(_ filter: CIFilter) {
         currentFilter = filter
         loadImage()
+        
+        filterCount += 1
+        if filterCount >= 25 {
+            requestReview()
+        }
     }
-    
-//    func save() {
-//        guard let processedImage = processedImage else { return }
-//        
-//        let imageSaver = ImageSaver()
-//        
-//        imageSaver.successHandler = {
-//            print("Success!")
-//        }
-//        
-//        imageSaver.errorHandler = {
-//            print("Oops! \($0.localizedDescription)")
-//        }
-//        
-//        imageSaver.writeToPhotoAlbum(image: processedImage)
-//    }
 }
 
 #Preview {
