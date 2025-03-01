6331c94d87d58fd4a5d67b677136ac0a0ebe9b70
Maxim Datskiy <datskiumax@gmail.com>
2025-03-01 11:26:29 -0800

    iOS 17 UPDATE - Days 96 - Part 5/5
    Sharing @Observable object through SwiftUI's environment

--- Day 96 - SnowSeeker/SnowSeeker/SnowSeeker/ContentView.swift
+++ Day 96 - SnowSeeker/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,30 +7,31 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var searchText = ""
-    let allNames = ["Max", "Kevin", "Rob", "John", "David", "Sarah", "Alex", "Emma", "Michael", "Liam"]
-    
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            allNames
-        } else {
-            allNames.filter { name in
-                name.localizedStandardContains(searchText)
-//                $0.localizedStandardContains(searchText)
-            }
-        }
-    }
+@Observable
+class Player {
+    var name = "Anonymous"
+    var highScore = 0
+}
+
+struct HighScoreView: View {
+    @Environment(Player.self) var player
     
     var body: some View {
-        NavigationStack {
-            
-            List(filteredNames, id: \.self) { name in
-                Text(name)
-            }
-            .searchable(text: $searchText, prompt: "Look for something...")
-            .navigationTitle("Searching")
+        @Bindable var player = player
+        
+        Stepper("High score: \(player.highScore)", value: $player.highScore)
+    }
+}
+
+struct ContentView: View {
+    @State private var player = Player()
+    
+    var body: some View {
+        VStack {
+            Text("Welcome!")
+            HighScoreView()
         }
+        .environment(player)
     }
 }
 
